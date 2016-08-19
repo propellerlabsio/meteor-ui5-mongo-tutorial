4e8ec8a409ec606c6abee910c25eeb247b25cf98
Peter Roehlen <peter.roehlen@gmail.com>
2016-08-20 01:12:28 +0700

    Remove temporary data creation

--- server/main.js
+++ server/main.js
@@ -1,9 +1,1 @@
 import { tasks } from '../imports/api/tasks.js';
-
-// Temporary code: Insert some tasks if the collection is empty
-const taskCount = tasks.find().count();
-if (!taskCount) {
-  tasks.insert({ text: 'This is mongo task 1' });
-  tasks.insert({ text: 'This is mongo task 2' });
-  tasks.insert({ text: 'This is mongo task 3' });
-}
